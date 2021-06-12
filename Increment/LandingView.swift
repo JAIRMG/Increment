04d2754835a1a7774bf31ce96e88d7807148d54f
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-06-12 12:39:44 -0500

    Adding landing page backgorund + Button

--- Increment/LandingView.swift
+++ Increment/LandingView.swift
@@ -5,13 +5,43 @@ import SwiftUI
 
 struct LandingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { proxy in
+            VStack {
+                Spacer().frame(height: proxy.size.height * 0.18)
+                Text("Increment")
+                    .font(.system(size: 64, weight: .medium))
+                    .foregroundColor(.white)
+                Spacer()
+                Button(action: {}) {
+                    HStack(spacing: 15) {
+                        Spacer()
+                        Image(systemName: "plus.circle")
+                        Text("Create a challenge")
+                        Spacer()
+                    }
+                    .font(.system(size: 24, weight: .semibold))
+                    .foregroundColor(.white)
+                }
+                .padding(.horizontal, 15)
+                .buttonStyle(PrimaryButtonStyle())
+            }.frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity
+            )
+            .background(
+                Image("pullup")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(Color.black.opacity(0.4))
+                    .frame(width: proxy.size.width)
+                    .edgesIgnoringSafeArea(.all)
+            )
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        LandingView()
+        LandingView().previewDevice("iPhone 11")
     }
 }
