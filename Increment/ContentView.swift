d4670b503d3f658893403d5a9e61b462da29b413
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-06-12 11:46:29 -0500

    First commit

---
+++ Increment/ContentView.swift
@@ -0,0 +1,17 @@
+// ContentView.swift, created on 12/06/21 by jair
+	
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,13 @@
+// IncrementApp.swift, created on 12/06/21 by jair
+	
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
