f22d067d00072a7f1b0d1eca18db75053b90b5b1
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-06-12 12:52:27 -0500

    Navigate to `Text` screen when tapping button

--- Increment/LandingView.swift
+++ Increment/LandingView.swift
@@ -1,42 +1,50 @@
 // ContentView.swift, created on 12/06/21 by jair
-	
+
 
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive = false
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Spacer().frame(height: proxy.size.height * 0.18)
-                Text("Increment")
-                    .font(.system(size: 64, weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action: {}) {
-                    HStack(spacing: 15) {
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                        Text("Create a challenge")
-                        Spacer()
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Spacer().frame(height: proxy.size.height * 0.18)
+                    Text("Increment")
+                        .font(.system(size: 64, weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: Text("navigated"), isActive: $isActive) {
+                        Button(action: {
+                            isActive = true
+                        }) {
+                            HStack(spacing: 15) {
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                Text("Create a challenge")
+                                Spacer()
+                            }
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.white)
+                        }
+                        .padding(.horizontal, 15)
+                        .buttonStyle(PrimaryButtonStyle())
                     }
-                    .font(.system(size: 24, weight: .semibold))
-                    .foregroundColor(.white)
-                }
-                .padding(.horizontal, 15)
-                .buttonStyle(PrimaryButtonStyle())
-            }.frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity
-            )
-            .background(
-                Image("pullup")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .overlay(Color.black.opacity(0.4))
-                    .frame(width: proxy.size.width)
-                    .edgesIgnoringSafeArea(.all)
-            )
+                }.frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity
+                )
+                .background(
+                    Image("pullup")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .overlay(Color.black.opacity(0.4))
+                        .frame(width: proxy.size.width)
+                        .edgesIgnoringSafeArea(.all)
+                )
+            }
         }
+        .accentColor(.primary)
     }
 }
 
