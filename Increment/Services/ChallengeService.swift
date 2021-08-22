da1a5395dfba94a45a4ad6346e4e61290298366a
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-08-21 23:32:43 -0500

    Delete challenge

--- Increment/Services/ChallengeService.swift
+++ Increment/Services/ChallengeService.swift
@@ -8,6 +8,7 @@ import FirebaseFirestoreSwift
 protocol ChallengeServiceProtocol {
     func create(_ challenge: Challenge) -> AnyPublisher<Void, IncrementError>
     func observeChallenges(userId: UserId) -> AnyPublisher<[Challenge], IncrementError>
+    func delete(_ challengeId: String) -> AnyPublisher<Void, IncrementError>
 }
 
 final class ChallengeService: ChallengeServiceProtocol {
@@ -31,7 +32,10 @@ final class ChallengeService: ChallengeServiceProtocol {
     }
     
     func observeChallenges(userId: UserId) -> AnyPublisher<[Challenge], IncrementError> {
-        let query = db.collection("challenges").whereField("userId", isEqualTo: userId)
+        let query = db.collection("challenges")
+            .whereField("userId", isEqualTo: userId)
+            .order(by: "startDate", descending: true)
+        
         return Publishers.QuerySnapshotPublisher(query: query)
             .flatMap { snapshot -> AnyPublisher<[Challenge], IncrementError> in
                 do {
@@ -46,5 +50,17 @@ final class ChallengeService: ChallengeServiceProtocol {
                         .eraseToAnyPublisher()
                 }
             }.eraseToAnyPublisher()
+    }
+    
+    func delete(_ challengeId: String) -> AnyPublisher<Void, IncrementError> {
+        return Future<Void, IncrementError> { promise in
+            self.db.collection("challenges").document(challengeId).delete { error in
+                if let error = error {
+                    promise(.failure(.default(description: error.localizedDescription)))
+                } else {
+                    promise(.success(()))
+                }
+            }
+        }.eraseToAnyPublisher()
     }
 }
